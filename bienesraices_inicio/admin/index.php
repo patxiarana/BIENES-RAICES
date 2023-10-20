19b9ebcbcacd7411c6ac393ebd71869b023a0cb1
patxi <patxiarana05@gmail.com>
2023-10-20 11:48:50 -0300

    form de creacion de propiedades

--- bienesraices_inicio/admin/index.php
+++ bienesraices_inicio/admin/index.php
@@ -7,9 +7,8 @@
 <main class="contenedor-seccion">
  <h1>Administrador de Bienes Raices</h1>
 
-
-
 <a href="admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+
 </main>
 
 <?php 
--- bienesraices_inicio/admin/propiedades/crear.php
+++ bienesraices_inicio/admin/propiedades/crear.php
@@ -8,6 +8,59 @@
  <h1>Crear</h1>
 
  <a href="/admin" class="boton boton-verde">Volver</a>
+
+
+ <form action="" class="formulario">
+
+<fieldset> 
+<legend>Informacion General</legend>
+
+<label for="titulo">Titulo:</label>
+<input type="text" id="titulo" placeholder="titulo de la propiedad...">
+
+
+<label for="precio">Precio:</label>
+<input type="number" id="precio" placeholder="precio de la propiedad...">
+
+<label for="imagen">imagen:</label>
+<input type="file" id="imagen" accept="image/jpeg, image/png">
+
+<label for="Descripcion">Descripcion</label>
+<textarea  id="descripcion" ></textarea>
+</fieldset>
+
+<fieldset>
+   <legend>Informacion de la Propiedad</legend>
+
+
+   <label for="habitaciones">Habitaciones:</label>
+   <input type="number" id="habitaciones" placeholder="ej 3 ..." min="1" max="9" >
+
+
+   
+   <label for="wc">Baños:</label>
+   <input type="number" id="wc" placeholder="ej 2 ..." min="1" max="9" >
+
+
+   <label for="estacionamiento">Estacionamiento:</label>
+   <input type="number" id="estacionamiento" placeholder="ej 2 ..." min="1" max="9" >
+
+</fieldset>
+
+
+<fieldset>
+   <legend>Vendedor</legend>
+
+   <select>
+   <option value="" disabled selected>--Seleccione--</option>
+      <option value="1">Patxi</option>
+      <option value="2">Karen</option>
+   </select>
+</fieldset>
+
+<input type="submit" value="Crear Propiedad" class="boton boton-verde">
+</form>
+
 </main>
 
 <?php 
