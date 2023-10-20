7f8820f5ca7ce4f65fffd1f1338ca457ddd729ed
patxi <patxiarana05@gmail.com>
2023-10-20 11:05:17 -0300

    Creando los archivos para las acciones del CRUD

---
+++ bienesraices_inicio/includes/templates/footer.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienes Raices</title>
+    <link rel="stylesheet" href="/build/css/app.css">
+</head>
+<body>
+<footer class="footer seccion">
+        <div class="contenedor-footer">
+            <nav class="navegacion">
+                <a href="nosotros.html">Nosotros</a>
+                <a href="anuncios.html">Anuncios</a>
+                <a href="blog.html">Blog</a>
+                <a href="contacto.html">Contacto</a>
+           </nav>
+     </div>
+     <p class="copyright">Todos los Derechos Reservados 2023</p>
+
+     <script src="build/js/bundle.min.js"></script>
+    </footer>
+</body>
+</html>
