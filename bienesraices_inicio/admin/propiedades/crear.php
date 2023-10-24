507d3578e9baef1a318abb583d95524df9a4b2fd
patxi <patxiarana05@gmail.com>
2023-10-24 12:23:08 -0300

    Creando primeras propiedades

--- bienesraices_inicio/admin/propiedades/crear.php
+++ bienesraices_inicio/admin/propiedades/crear.php
@@ -5,6 +5,36 @@ require '../../includes/config/database.php';
 
   $db  = conectarDB() ; 
 
+  echo  "<pre>" ; 
+
+  var_dump($_POST) ; 
+
+  echo "</pre>" ; 
+
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+$titulo = $_POST['titulo'] ; 
+$precio = floatval($_POST['precio']);
+$descripcion = $_POST['descripcion'] ; 
+$habitaciones= intval($_POST['habitaciones']); 
+$wc = intval($_POST['wc'] ); 
+$estacionamiento = intval($_POST['estacionamiento'] ); 
+$vendedores_id = isset($_POST['vendedores_id']) ? intval($_POST['vendedores_id']) : 1;
+
+//Insertar en la base de datos 
+
+
+
+$query = " INSERT INTO propiedades (titulo,precio,descripcion,habitaciones,wc,estacionamiento,vendedores_id) VALUES ('$titulo', '$precio', '$descripcion','$habitaciones','$wc','$estacionamiento','$vendedores_id') ";
+
+//echo $query ; 
+
+$resultado = mysqli_query($db, $query) ; 
+
+if($resultado) {
+   echo 'insertado correctamente';
+} 
+  }
+
    require '../../includes/funciones.php' ; 
    incluirTemplate('header') ; 
 ?>
@@ -16,23 +46,24 @@ require '../../includes/config/database.php';
  <a href="/admin" class="boton boton-verde">Volver</a>
 
 
- <form class="formulario">
+ <form class="formulario" method="POST" action="/admin/propiedades/crear.php">
 
 <fieldset> 
 <legend>Informacion General</legend>
 
 <label for="titulo">Titulo:</label>
-<input type="text" id="titulo" placeholder="titulo de la propiedad...">
+<input type="text" id="titulo" name="titulo" placeholder="titulo de la propiedad...">
 
 
 <label for="precio">Precio:</label>
-<input type="number" id="precio" placeholder="precio de la propiedad...">
+
+<input  type="number" id="precio" name="precio" placeholder="precio de la propiedad...">
 
 <label for="imagen">imagen:</label>
 <input type="file" id="imagen" accept="image/jpeg, image/png">
 
 <label for="Descripcion">Descripcion</label>
-<textarea  id="descripcion" ></textarea>
+<textarea  id="descripcion" name="descripcion" ></textarea>
 </fieldset>
 
 <fieldset>
@@ -40,16 +71,16 @@ require '../../includes/config/database.php';
 
 
    <label for="habitaciones">Habitaciones:</label>
-   <input type="number" id="habitaciones" placeholder="ej 3 ..." min="1" max="9" >
+   <input type="number" name="habitaciones" id="habitaciones" placeholder="ej 3 ..." min="1" max="9" >
 
 
    
    <label for="wc">Baños:</label>
-   <input type="number" id="wc" placeholder="ej 2 ..." min="1" max="9" >
+   <input type="number" id="wc" name="wc" placeholder="ej 2 ..." min="1" max="9" >
 
 
    <label for="estacionamiento">Estacionamiento:</label>
-   <input type="number" id="estacionamiento" placeholder="ej 2 ..." min="1" max="9" >
+   <input type="number" name="estacionamiento" id="estacionamiento" placeholder="ej 2 ..." min="1" max="9" >
 
 </fieldset>
 
@@ -57,10 +88,10 @@ require '../../includes/config/database.php';
 <fieldset>
    <legend>Vendedor</legend>
 
-   <select>
+   <select name="vendedores_id">
    <option value="" disabled selected>--Seleccione--</option>
       <option value="1">Patxi</option>
-      <option value="2">Karen</option>
+      <option value="2" >Karen</option>
    </select>
 </fieldset>
 
