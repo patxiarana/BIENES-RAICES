34faed587fd612211e8d771ef2e76967eca8540d
patxi <patxiarana05@gmail.com>
2023-10-26 10:14:11 -0300

    Sanitizando entrada de datos

--- bienesraices_inicio/admin/propiedades/crear.php
+++ bienesraices_inicio/admin/propiedades/crear.php
@@ -36,13 +36,22 @@ $vendedores_id =  '';
 
   if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-$titulo = $_POST['titulo'] ; 
-$precio = ($_POST['precio']);
-$descripcion = $_POST['descripcion'] ; 
-$habitaciones= ($_POST['habitaciones']); 
-$wc = ($_POST['wc'] ); 
-$estacionamiento = ($_POST['estacionamiento'] ); 
-$vendedores_id = $_POST['vendedores_id'] ;
+
+$numero = "1hola" ; 
+$numero2 = 1 ; 
+
+//Sanitizar 
+
+
+   exit ; 
+
+$titulo =mysqli_real_escape_string($db,$_POST['titulo']) ; 
+$precio = mysqli_real_escape_string($db,$_POST['precio']);
+$descripcion = mysqli_real_escape_string($db,$_POST['descripcion']) ; 
+$habitaciones= mysqli_real_escape_string($db,$_POST['habitaciones']); 
+$wc = mysqli_real_escape_string($db,$_POST['wc'] ); 
+$estacionamiento = mysqli_real_escape_string($db,$_POST['estacionamiento'] ); 
+$vendedores_id = mysqli_real_escape_string($db,$_POST['vendedores_id']) ;
 $creado =date('Y/m/d') ; 
 
 if(!$titulo) {
