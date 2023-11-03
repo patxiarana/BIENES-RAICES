5be2b54366284b5e8607ae150c008e7f168c0e19
patxi <patxiarana05@gmail.com>
2023-11-03 10:43:35 -0300

    creando lista para motrar las propiedades

--- bienesraices_inicio/admin/index.php
+++ bienesraices_inicio/admin/index.php
@@ -12,6 +12,31 @@ $resultado = $_GET['resultado'] ?? null ;
  <?php endif ;?>
 <a href="admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
 
+
+ <table class="propiedades">
+   <thead> 
+      <tr> 
+         <th>ID</th>
+         <th>Titulo</th>
+         <th>Imagen</th>
+         <th>Precio</th>
+         <th>Acciones</th>
+      </tr>
+  </thead>
+<tbody> 
+   <tr>
+      <td>1</td>
+      <td>Casa en la playa</td>
+      <td> <img src="/imagenes/f6b2909c70bcaa92f4c0a43dfb978f45.jpg" class="imagen-tabla"/>  </td>
+      <td>$150000</td>
+      <td> <a href="#"class="boton-rojo-block">Eliminar</a>
+      <a href="#" class="boton-amarillo-block">Actualizar</a>
+   </td>
+   </tr>
+</tbody>
+
+ </table>
+
 </main>
 
 <?php 
