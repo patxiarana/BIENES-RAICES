db1411721784f858c816a3940ba50708ac3c7397
patxi <patxiarana05@gmail.com>
2023-11-07 12:49:22 -0300

    propiedades de la pagina principal y anuncios dinamicos

--- bienesraices_inicio/index.php
+++ bienesraices_inicio/index.php
@@ -32,92 +32,11 @@ require 'includes/funciones.php' ;
     </main>
  <section class="seccion contenedor">
  <h2>Casas y Depas en Venta</h2>
- <div class="contenedor-anuncios">
-    <div class="anuncio">
-        <picture>
-            <source srcset="build/img/anuncio1.webp" type="image/webp">
-            <source srcset="build/img/anuncio1.jpg" type="image/jpeg">
-            <img  loading="lazy" src="build/img/anuncio1.jpg" alt="anuncio">
-        </picture>
-        <div class="contenido-anuncio">
-            <h3>Casa de lujo en el lago</h3>
-            <p>Casa en el lago con excelente vista, acabados de lujo a un excelente precio</p>
-            <p class="precop">$3,000,000</p>
-            <ul class="iconos-caracteristicas">
-             <li>
-                <img  class="icono" loading="lazy" src="build/img/icono_wc.svg"   alt="icono wc">
-                <p>3</p>
-             </li>
-             <li>
-                <img  class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg"   alt="icono estacionamiento">
-                <p>3</p>
-             </li>
-             <li>
-                <img  class="icono" loading="lazy" src="build/img/icono_dormitorio.svg"   alt="icono dormitorio">
-                <p>4</p>
-             </li>
-            </ul>
-               
-        <a href="anuncio.html" class="boton-amarillo-block"> Ver Propiedad</a>
-        </div>
-    </div>
-    <div class="anuncio">
-        <picture>
-            <source srcset="build/img/anuncio2.webp" type="image/webp">
-            <source srcset="build/img/anuncio2.jpg" type="image/jpeg">
-            <img  loading="lazy" src="build/img/anuncio1.jpg" alt="anuncio">
-        </picture>
-        <div class="contenido-anuncio">
-            <h3>Casa terminados de lujo</h3>
-            <p>Casa en el lago con excelente vista, acabados de lujo a un excelente precio</p>
-            <p class="precop">$3,000,000</p>
-            <ul class="iconos-caracteristicas">
-             <li>
-                <img  class="icono"  loading="lazy" src="build/img/icono_wc.svg"   alt="icono wc">
-                <p>3</p>
-             </li>
-             <li>
-                <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg"   alt="icono estacionamiento">
-                <p>3</p>
-             </li>
-             <li>
-                <img class="icono"  loading="lazy" src="build/img/icono_dormitorio.svg"   alt="icono dormitorio">
-                <p>4</p>
-             </li>
-            </ul>
-               
-        <a href="anuncio.html" class="boton-amarillo-block"> Ver Propiedad</a>
-        </div>
-    </div>
-    <div class="anuncio">
-        <picture>
-            <source srcset="build/img/anuncio3.webp" type="image/webp">
-            <source srcset="build/img/anuncio3.jpg" type="image/jpeg">
-            <img  loading="lazy" src="build/img/anuncio1.jpg" alt="anuncio">
-        </picture>
-        <div class="contenido-anuncio">
-            <h3>Casa con pileta</h3>
-            <p>Casa en el lago con excelente vista, acabados de lujo a un excelente precio</p>
-            <p class="precop">$3,000,000</p>
-            <ul class="iconos-caracteristicas">
-             <li>
-                <img class="icono" loading="lazy" src="build/img/icono_wc.svg"   alt="icono wc">
-                <p>3</p>
-             </li>
-             <li>
-                <img  class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg"   alt="icono estacionamiento">
-                <p>3</p>
-             </li>
-             <li>
-                <img  class="icono" loading="lazy" src="build/img/icono_dormitorio.svg"   alt="icono dormitorio">
-                <p>4</p>
-             </li>
-            </ul>
-               
-        <a href="anuncio.html" class="boton-amarillo-block"> Ver Propiedad</a>
-        </div>
-    </div>
- </div>
+
+ <?php     
+ $limite = 3 ; 
+ include 'includes/templates/anuncios.php'
+ ?>
 
  <div class="alinear-derecha">
     <a href="anuncios.html" class="boton-verde">Ver Todas</a>
